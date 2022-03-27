4b6dee321bcc2f7df636240f1468b9d070db0a38
Matt Bulger <mcbulger2@gmail.com>
2022-03-26 19:44:52 -0700

    Fix installation bug that could wipe your home directory

--- groupproject/setup.sh
+++ groupproject/setup.sh
@@ -1,17 +1,17 @@
 # Stop postgresql and delete the existing database
-pg_ctl -D ~/$1 stop
-rm -rf ~/$1
+pg_ctl -D ~/cse412groupproject stop
+rm -rf ~/cse412groupproject
 
 # Set parameters and initialize new database
-export PGPORT=$2
+export PGPORT=$1
 export PGHOST=/tmp
-initdb ~/$1
-pg_ctl -D ~/$1 -o '-k /tmp' start
-createdb $1
+initdb ~/cse412groupproject
+pg_ctl -D ~/cse412groupproject -o '-k /tmp' start
+createdb cse412groupproject
 
 #  Create the database schema using DDL
 
-psql -U $USER -d $1 -c "CREATE TABLE Profile (
+psql -U $USER -d cse412groupproject -c "CREATE TABLE Profile (
 	user_id SERIAL PRIMARY KEY,
 	username TEXT NOT NULL UNIQUE,
 	name TEXT,
@@ -19,11 +19,11 @@ psql -U $USER -d $1 -c "CREATE TABLE Profile (
 	profile_picture BYTEA,
 	pw_hash BYTEA NOT NULL
 );"
-psql -U $USER -d $1 -c "CREATE TABLE Conversation (
+psql -U $USER -d cse412groupproject -c "CREATE TABLE Conversation (
 	conversation_id SERIAL PRIMARY KEY,
 	name TEXT
 );"
-psql -U $USER -d $1 -c "CREATE TABLE ConversationMessage (
+psql -U $USER -d cse412groupproject -c "CREATE TABLE ConversationMessage (
 	message_id SERIAL PRIMARY KEY,
 	content TEXT,
 	timestamp TIMESTAMP NOT NULL DEFAULT NOW(),
@@ -31,12 +31,12 @@ psql -U $USER -d $1 -c "CREATE TABLE ConversationMessage (
 	sender_id INT NOT NULL REFERENCES Profile (user_id) ON DELETE CASCADE,
 	conversation_id INT REFERENCES Conversation(conversation_id) ON DELETE CASCADE
 );"
-psql -U $USER -d $1 -c "CREATE TABLE ConversationUsers (
+psql -U $USER -d cse412groupproject -c "CREATE TABLE ConversationUsers (
 	conversation_id INT NOT NULL REFERENCES Conversation ON DELETE CASCADE,
 	user_id INT NOT NULL REFERENCES Profile ON DELETE CASCADE,
 	PRIMARY KEY (conversation_id, user_id)
 );"
-psql -U $USER -d $1 -c "CREATE TABLE Friend (
+psql -U $USER -d cse412groupproject -c "CREATE TABLE Friend (
 	friender_id INT NOT NULL REFERENCES Profile (user_id) ON DELETE CASCADE,
 	friendee_id INT NOT NULL REFERENCES Profile (user_id) ON DELETE CASCADE,
 	PRIMARY KEY (friender_id, friendee_id)
@@ -44,7 +44,7 @@ psql -U $USER -d $1 -c "CREATE TABLE Friend (
 
 # Insert synthetic data into the database
 
-psql -U $USER -d $1 -c "INSERT INTO Profile (username, name, birthday, pw_hash) VALUES 
+psql -U $USER -d cse412groupproject -c "INSERT INTO Profile (username, name, birthday, pw_hash) VALUES 
     ('jdoe', 'John Doe', '2000-11-30', '\$2a\$12\$hFEMEP9h5to0FOx..2Vbnu/p3Thq5T9bnUGpr3dc6Vq7woLUXSkXO'),
     ('jfletcher', 'Julie Fletcher', '1957-05-22', '\$2a\$12\$hFEMEP9h5to0FOx..2Vbnu/p3Thq5T9bnUGpr3dc6Vq7woLUXSkXO'),
     ('philgil', 'Phil Gilbreath', '1997-06-06', '\$2a\$12\$hFEMEP9h5to0FOx..2Vbnu/p3Thq5T9bnUGpr3dc6Vq7woLUXSkXO'),
@@ -78,25 +78,25 @@ psql -U $USER -d $1 -c "INSERT INTO Profile (username, name, birthday, pw_hash) 
 ;"
 
 # Friendships
-psql -U $USER -d $1 -c "INSERT INTO Friend VALUES (1, 2), (2, 1), (1, 3), (3, 1), (2, 3), (3, 2);"      # First 3 users are all friends with each other
-psql -U $USER -d $1 -c "INSERT INTO Friend VALUES (4, 1), (5, 1), (4, 5), (5, 4);"                      # 4 and 5 are friends with 1 but not vice versa, and friends with each other
-psql -U $USER -d $1 -c "INSERT INTO Friend VALUES (6, 2), (2, 6), (6, 3), (4, 6);"
-psql -U $USER -d $1 -c "INSERT INTO Friend VALUES (7, 6), (6, 7), (7, 5), (5, 7);"
-psql -U $USER -d $1 -c "INSERT INTO Friend VALUES (8, 9), (8, 10), (8, 11), (8, 12);"
-psql -U $USER -d $1 -c "INSERT INTO Friend VALUES (9, 8), (9, 10), (9, 11), (9, 12);"
-psql -U $USER -d $1 -c "INSERT INTO Friend VALUES (10, 11), (10, 8), (10, 9);"
-psql -U $USER -d $1 -c "INSERT INTO Friend VALUES (11, 10), (11, 8), (11, 9);"
-psql -U $USER -d $1 -c "INSERT INTO Friend VALUES (12, 10), (12, 8), (12, 9);"
-psql -U $USER -d $1 -c "INSERT INTO Friend VALUES (30, 29), (29, 30);"
-psql -U $USER -d $1 -c "INSERT INTO Friend VALUES (29, 28), (28, 29);"
-psql -U $USER -d $1 -c "INSERT INTO Friend VALUES (28, 27), (27, 28);"
-psql -U $USER -d $1 -c "INSERT INTO Friend VALUES (27, 26), (26, 27);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO Friend VALUES (1, 2), (2, 1), (1, 3), (3, 1), (2, 3), (3, 2);"      # First 3 users are all friends with each other
+psql -U $USER -d cse412groupproject -c "INSERT INTO Friend VALUES (4, 1), (5, 1), (4, 5), (5, 4);"                      # 4 and 5 are friends with 1 but not vice versa, and friends with each other
+psql -U $USER -d cse412groupproject -c "INSERT INTO Friend VALUES (6, 2), (2, 6), (6, 3), (4, 6);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO Friend VALUES (7, 6), (6, 7), (7, 5), (5, 7);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO Friend VALUES (8, 9), (8, 10), (8, 11), (8, 12);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO Friend VALUES (9, 8), (9, 10), (9, 11), (9, 12);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO Friend VALUES (10, 11), (10, 8), (10, 9);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO Friend VALUES (11, 10), (11, 8), (11, 9);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO Friend VALUES (12, 10), (12, 8), (12, 9);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO Friend VALUES (30, 29), (29, 30);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO Friend VALUES (29, 28), (28, 29);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO Friend VALUES (28, 27), (27, 28);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO Friend VALUES (27, 26), (26, 27);"
 
 
 # Create conversations
-psql -U $USER -d $1 -c "INSERT INTO Conversation (name) VALUES ('Group Convo 1');"
-psql -U $USER -d $1 -c "INSERT INTO ConversationUsers VALUES (1, 1), (1, 2), (1, 3);"
-psql -U $USER -d $1 -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
+psql -U $USER -d cse412groupproject -c "INSERT INTO Conversation (name) VALUES ('Group Convo 1');"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationUsers VALUES (1, 1), (1, 2), (1, 3);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
     ('Hello, World!', 1, 1),
     ('Hello, back!', 2, 1),
     ('Same here, hello', 3, 1),
@@ -105,33 +105,33 @@ psql -U $USER -d $1 -c "INSERT INTO ConversationMessage (content, sender_id, con
     ('yes', 2, 1)
 ;"
 
-psql -U $USER -d $1 -c "INSERT INTO Conversation (name) VALUES ('Normal Convo 1');"
-psql -U $USER -d $1 -c "INSERT INTO ConversationUsers VALUES (2, 1), (2, 2);"
-psql -U $USER -d $1 -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
+psql -U $USER -d cse412groupproject -c "INSERT INTO Conversation (name) VALUES ('Normal Convo 1');"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationUsers VALUES (2, 1), (2, 2);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
     ('No comfort do written conduct at prevent manners on.', 2, 2),
     ('Alone visit use these smart rooms ham. No waiting in on enjoyed placing it inquiry.', 1, 2),
     ('No comfort do written conduct at prevent manners on.', 2, 2),
     ('By in no ecstatic wondered disposal my speaking. Direct wholly valley or uneasy it at really.', 1, 2)
 ;"
 
-psql -U $USER -d $1 -c "INSERT INTO Conversation (name) VALUES ('Normal Convo 2');"
-psql -U $USER -d $1 -c "INSERT INTO ConversationUsers VALUES (3, 7), (2, 5);"
-psql -U $USER -d $1 -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
+psql -U $USER -d cse412groupproject -c "INSERT INTO Conversation (name) VALUES ('Normal Convo 2');"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationUsers VALUES (3, 7), (2, 5);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
     ('ts sometimes her behaviour are contented. ', 5, 3),
     ('Her companions instrument set estimating sex remarkably solicitude motionless.', 7, 3),
     ('Remain lively hardly needed at do by. ', 7, 3)
 ;"
 
-psql -U $USER -d $1 -c "INSERT INTO Conversation (name) VALUES ('Normal Convo 3');"
-psql -U $USER -d $1 -c "INSERT INTO ConversationUsers VALUES (4, 2), (4, 4);"
-psql -U $USER -d $1 -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
+psql -U $USER -d cse412groupproject -c "INSERT INTO Conversation (name) VALUES ('Normal Convo 3');"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationUsers VALUES (4, 2), (4, 4);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
     ('This is a private conversation between us', 2, 4),
     ('Sounds good, its just us then', 4, 4)
 ;"
 
-psql -U $USER -d $1 -c "INSERT INTO Conversation (name) VALUES ('Normal Convo 4');"
-psql -U $USER -d $1 -c "INSERT INTO ConversationUsers VALUES (5, 8), (5, 7);"
-psql -U $USER -d $1 -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
+psql -U $USER -d cse412groupproject -c "INSERT INTO Conversation (name) VALUES ('Normal Convo 4');"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationUsers VALUES (5, 8), (5, 7);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
     ('Same an quit most an. Admitting an mr disposing sportsmen.', 8, 5),
     ('New the her nor case that lady paid read. Invitation friendship travelling eat everything the out two. Shy you who scarcely expenses debating hastened resolved. Always polite moment on is warmth spirit it to hearts. Downs those still witty an balls so chief so. Moment an little remain no up lively no. Way brought may off our regular country towards adapted cheered. ', 7, 5),
     ('Placing assured be', 8, 5),
@@ -139,44 +139,44 @@ psql -U $USER -d $1 -c "INSERT INTO ConversationMessage (content, sender_id, con
     ('By an outlived insisted procured improved am. ', 8, 5)
 ;"
 
-psql -U $USER -d $1 -c "INSERT INTO Conversation (name) VALUES ('Normal Convo 5');"
-psql -U $USER -d $1 -c "INSERT INTO ConversationUsers VALUES (6, 30), (6, 29);"
-psql -U $USER -d $1 -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
+psql -U $USER -d cse412groupproject -c "INSERT INTO Conversation (name) VALUES ('Normal Convo 5');"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationUsers VALUES (6, 30), (6, 29);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
     ('This is a private conversation between us', 30, 6),
     ('Sounds good, its just us then', 29, 6)
 ;"
 
-psql -U $USER -d $1 -c "INSERT INTO Conversation (name) VALUES ('Normal Convo 6');"
-psql -U $USER -d $1 -c "INSERT INTO ConversationUsers VALUES (7, 29), (7, 28);"
-psql -U $USER -d $1 -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
+psql -U $USER -d cse412groupproject -c "INSERT INTO Conversation (name) VALUES ('Normal Convo 6');"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationUsers VALUES (7, 29), (7, 28);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
     ('This is a private conversation between us', 29, 7),
     ('Sounds good, its just us then', 28, 7)
 ;"
 
-psql -U $USER -d $1 -c "INSERT INTO Conversation (name) VALUES ('Group Convo 2');"
-psql -U $USER -d $1 -c "INSERT INTO ConversationUsers VALUES (8, 9), (8, 10), (8, 11);"
-psql -U $USER -d $1 -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
+psql -U $USER -d cse412groupproject -c "INSERT INTO Conversation (name) VALUES ('Group Convo 2');"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationUsers VALUES (8, 9), (8, 10), (8, 11);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
     ('message1', 8, 8),
     ('message2', 9, 8),
     ('message2', 10, 8)
 ;"
 
-psql -U $USER -d $1 -c "INSERT INTO Conversation (name) VALUES ('Normal Convo 7');"
-psql -U $USER -d $1 -c "INSERT INTO ConversationUsers VALUES (9, 27), (9, 26);"
-psql -U $USER -d $1 -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
+psql -U $USER -d cse412groupproject -c "INSERT INTO Conversation (name) VALUES ('Normal Convo 7');"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationUsers VALUES (9, 27), (9, 26);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
     ('This is a private conversation between us', 27, 7),
     ('Sounds good, its just us then', 26, 7)
 ;"
 
-psql -U $USER -d $1 -c "INSERT INTO Conversation (name) VALUES ('Normal Convo 8');"
-psql -U $USER -d $1 -c "INSERT INTO ConversationUsers VALUES (10, 26), (10, 25);"
-psql -U $USER -d $1 -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
+psql -U $USER -d cse412groupproject -c "INSERT INTO Conversation (name) VALUES ('Normal Convo 8');"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationUsers VALUES (10, 26), (10, 25);"
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationMessage (content, sender_id, conversation_id) VALUES 
     ('This is a private conversation between us', 26, 7),
     ('Sounds good, its just us then', 25, 7)
 ;"
 
 # Social Feed Messages
-psql -U $USER -d $1 -c "INSERT INTO ConversationMessage (content, sender_id, isfeedmessage) VALUES
+psql -U $USER -d cse412groupproject -c "INSERT INTO ConversationMessage (content, sender_id, isfeedmessage) VALUES
     ('This is my first feed message', 1, 1),
     ('I like this messaging service!', 2, 1),
     ('[Insert Political Hot Take]', 3, 1),
@@ -193,4 +193,4 @@ psql -U $USER -d $1 -c "INSERT INTO ConversationMessage (content, sender_id, isf
 ;"
 
 # Finally, open a psql command prompt
-psql -p $2 -U $USER $1
+psql -p $1 -U $USER cse412groupproject
